2e3f5894363d42822c4304ca8c9ed21cd848796a
Maddie Kreamer <maddiekreamer@gmail.com>
2018-08-29 16:01:35 -0600

    Quotes, images, and button updated.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Ron Image" src="./assets/ron.webp">
+    <quote v-for='(quote, index) in ronsWisdom' v-bind:key='"quote"+index' v-bind:ronsWisdom='quote'/>
+    <button v-on:click='getQuotes'>NEW QUOTE</button> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import quote from "./components/quote";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    quote
+  },
+  data() {
+    return {
+      ronsWisdom: null
+    };
+  },
+  props: [],
+  methods: {
+    getQuotes() {
+      fetch("http://ron-swanson-quotes.herokuapp.com/v2/quotes/")
+        .then(resp => resp.json())
+        .then(resp => (this.ronsWisdom = resp));
+    }
+  },
+  mounted() {
+    this.getQuotes();
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  color: red;
+  width: 10vw;
+  height: 5vw;
+  font-size: 15px;
 }
 </style>
---
+++ src/components/quote.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ronsWisdom}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "quote",
+    props: ['ronsWisdom']
+}
+</script>
+
+<style>
+</style>
+
+
